3817e78b609817c18e63f4d3204964bf4b86a125
AdityaMuhK <adityamuhammadk@gmail.com>
2023-09-12 14:11:47 +0700

    Menambahkan CRUD ke dalam folder relasi

--- relasi/delete.php
+++ relasi/delete.php
@@ -1,13 +1,13 @@
 <?php
-    include 'connect.php';
-    if (isset($_GET['id'])) {
-        $id = $_GET['id'];
-        $sql = "delete from sekolah where id=$id";
-        $result = mysqli_query($conn, $sql);
-        if ($result) {
-            header('location:siswa.php');
-        } else {
-            die($conn->connect_error);
-        }
+include 'connect.php';
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $sql = "delete from siswa where id_siswa=$id";
+    $result = mysqli_query($conn, $sql);
+    if ($result) {
+        header('location:siswa.php');
+    } else {
+        die($conn->connect_error);
     }
+}
 ?>
